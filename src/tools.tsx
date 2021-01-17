05a39d613a467ed0ee8dc9374f6c22c400fc9b75
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2021-01-17 23:00:22 +0100

    Created simple formatter

--- src/tools.tsx
+++ src/tools.tsx
@@ -1,11 +1,17 @@
 import {Tool} from "./Tool";
 import JwtDecoder from "./tools/JwtDecoder";
+import JsonFormatter from "./tools/JsonFormatter";
 
 const tools: Tool[] = [
     {
         id: "jwt-decoder",
         name: "JWT decoder",
         render: () => <JwtDecoder/>
+    },
+    {
+        id: "json-formatter",
+        name: "JSON formatter",
+        render: () => <JsonFormatter/>
     }
 ];
 
---
+++ src/tools/JsonFormatter.tsx
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from 'react';
+import {Grid, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
+
+const JSON_FIELD_ROWS = 24;
+
+const JsonFormatter = () => {
+    const [rawJson, setRawJson] = useState<string>("");
+    const [parsedJson, setParsedJson] = useState<any>();
+    const [formattedJson, setFormattedJson] = useState<string>("");
+    const [parsingError, setParsingError] = useState<string>("");
+    const [template, setTemplate] = useState<string>("2")
+
+    useEffect(() => {
+        if (!parsedJson) {
+            setFormattedJson("");
+            return;
+        }
+        setFormattedJson(JSON.stringify(parsedJson, null, +template));
+    }, [parsedJson, template])
+
+    const handleRawJsonChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const value = event.target.value;
+        setRawJson(value);
+
+        if (!value) {
+            setParsingError("");
+            setParsedJson(undefined);
+        }
+
+        let parsedValue;
+        try {
+            parsedValue = JSON.parse(value);
+        } catch (error) {
+            setParsingError(error.message);
+            setParsedJson(undefined);
+            return;
+        }
+
+        setParsingError("");
+        setParsedJson(parsedValue);
+    };
+
+    return (
+        <Grid container direction="column" spacing={2}>
+            <Grid container spacing={2} item>
+                <Grid item xs={6}>
+                    <InputLabel id="json-template-select-label">JSON template</InputLabel>
+                    <Select
+                        labelId="json-template-select-label"
+                        id="json-template-select"
+                        fullWidth
+                        value={template}
+                        onChange={event => setTemplate(event.target.value as string)}
+                    >
+                        <MenuItem value="2">2 space tab</MenuItem>
+                        <MenuItem value="3">3 space tab</MenuItem>
+                        <MenuItem value="4">4 space tab</MenuItem>
+                        <MenuItem value="0">Compact</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={6}>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} item>
+                <Grid item xs={6}>
+                    <TextField
+                        label="Raw JSON"
+                        multiline
+                        fullWidth
+                        rows={JSON_FIELD_ROWS}
+                        variant="outlined"
+                        value={rawJson}
+                        onChange={handleRawJsonChange}
+                        error={!!parsingError}
+                        helperText={parsingError}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        label="Formatted JSON"
+                        multiline
+                        fullWidth
+                        rows={JSON_FIELD_ROWS}
+                        variant="outlined"
+                        value={formattedJson}
+                        onChange={() => {
+                            // read only text field
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default JsonFormatter;
