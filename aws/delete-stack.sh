1b2b919065b06acbb94056de5ea92068c180c045
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2021-04-02 16:35:20 +0200

    Deleted AWS related scripts

--- aws/delete-stack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-s3_bucket_name=$(aws cloudformation  describe-stacks --stack-name dev-toolbox --no-cli-pager --output json | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="S3BucketName").OutputValue')
-aws s3 rm s3://"$s3_bucket_name" --recursive
-aws cloudformation delete-stack --stack-name dev-toolbox --no-cli-pager
